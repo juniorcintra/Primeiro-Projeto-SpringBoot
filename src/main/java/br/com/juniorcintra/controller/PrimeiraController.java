7af1751714756a419623e3a5aa46754cb5742591
juniorcintra <junior.cintra.developer@gmail.com>
2025-02-12 03:01:06 -0300

    rotas GET e POST

--- src/main/java/br/com/juniorcintra/controller/PrimeiraController.java
+++ src/main/java/br/com/juniorcintra/controller/PrimeiraController.java
@@ -4,6 +4,9 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +29,16 @@ public class PrimeiraController {
     return "O id da rota metodoComQueryParams é: " + allParams.entrySet();
   }
   
+  @PostMapping("/metodoComBody")
+  public String metodoComBody(@RequestBody Usuario usuario ) {
+    return "O nome do usuario é: " + usuario.name();
+  }
+
+  @PostMapping("/metodoComListHeaders")
+  public String metodoComBody(@RequestHeader Map<String, String> headers) {
+    return "O nome do usuario é: " + headers.entrySet();
+  }
+
+  record Usuario(String id, String name) {}
 }
 
