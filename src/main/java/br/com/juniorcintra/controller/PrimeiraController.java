b8e265d62b1288d9ad3da29a1fa8a22ba2712a43
juniorcintra <junior.cintra.developer@gmail.com>
2025-02-12 02:42:39 -0300

    metodos get e params

---
+++ src/main/java/br/com/juniorcintra/controller/PrimeiraController.java
@@ -0,0 +1,30 @@
+package br.com.juniorcintra.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class PrimeiraController {
+
+  @GetMapping("/{id}")
+  public String hello(@PathVariable String id) {
+    return "O id é: " + id;
+  }
+
+  @GetMapping("/metodoComQueryParams")
+  public String metodoComQueryParams(@RequestParam String id) {
+    return "O id da rota metodoComQueryParams é: " + id;
+  }
+  @GetMapping("/metodoComQueryParams2")
+  public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
+    return "O id da rota metodoComQueryParams é: " + allParams.entrySet();
+  }
+  
+}
+
--- src/main/java/br/com/juniorcintra/primeiro_projeto_springboot/PrimeiroProjetoSpringbootApplication.java
+++ src/main/java/br/com/juniorcintra/primeiro_projeto_springboot/PrimeiroProjetoSpringbootApplication.java
@@ -2,8 +2,10 @@ package br.com.juniorcintra.primeiro_projeto_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.juniorcintra")
 public class PrimeiroProjetoSpringbootApplication {
 
 	public static void main(String[] args) {
